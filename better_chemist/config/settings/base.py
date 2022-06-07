0265c85edb8e5f4eeb78057ca6a096a7181e7231
Luke Greenberg <lukegreen.dev@gmail.com>
2022-06-07 16:56:35 +0200

    Backend: #1 Add models for scripts

--- better_chemist/config/settings/base.py
+++ better_chemist/config/settings/base.py
@@ -79,7 +79,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "better_chemist.users",
-    # Your stuff: custom apps go here
+    "script",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
--- better_chemist/script/models.py
+++ better_chemist/script/models.py
@@ -10,5 +10,9 @@ class Script(models.Model):
     Model used to store scripts that are sent by other sources in order for better verification later on
     """
 
-    
 
+    created = models.DateField(auto_now=True)
+    file = models.FileField(upload_to='script_files/')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    max_redeems = models.IntegerField()
+    redeemed_counter = models.IntegerField(default=0)
